abf9e65decd1885974950cead0edbc2e97259ca1
lukboy223 <ltvanekeren@gmail.com>
2024-09-23 15:26:12 +0200

    les 13

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -16,4 +16,8 @@ class Job extends Model
     {
         return $this->belongsTo(Employer::class);
     }
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
+    }
 }
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,13 +2,16 @@
     <x-slot:heading>
         Job listing
     </x-slot:heading>
-    <ul>
-    @foreach ($jobs as $job)
-        <li>
-            <a href="/job/{{ $job['id']}}" class="underline">
+    <div class="space-y-4">
+        @foreach ($jobs as $job)
+
+        <a href="/job/{{ $job['id']}}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+            <div class="font-bold text-blue-500">{{ $job->employer->name}}</div>
+            <div>
                 <strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year
-            </a>
-        </li>
-    @endforeach
-</ul>
+            </div>
+        </a>
+
+        @endforeach
+    </div>
 </x-layout>
