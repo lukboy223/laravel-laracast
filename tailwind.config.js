5ebc5ff09f5539100103c30de12280b032427f88
lukboy223 <ltvanekeren@gmail.com>
2024-10-21 16:06:35 +0200

    les 26

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'mainColor': "rgb(0,100,100)"
+      }
+    },
+  },
+  plugins: [],
+}
+
